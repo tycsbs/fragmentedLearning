e3a06409cbe6a2aeaefe9ae950a9e3ecd566e688
tycsbs <tutebaisong@163.com>
2018-05-30 00:35:36 +0800

    change projict to vue stack

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <background></background>
+  </div>
+</template>
+
+<script>
+import Background from './components/Background'
+
+export default {
+  name: 'App',
+  components: {
+    Background
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
