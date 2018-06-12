449312d165614632ee5b505fe37b1315cbb9b58f
tycsbs <tutebaisong@163.com>
2018-06-13 00:21:41 +0800

    submenu code format

--- src/components/bsHeader/bsHeader.vue
+++ src/components/bsHeader/bsHeader.vue
@@ -3,43 +3,11 @@
        <i-menu mode="horizontal"
         :theme="theme"
         @on-select="selectItem">
-        <menu-item name="1">
+        <menu-item  v-for='(item, index) in menuList' :key='index'
+          :name="item.name">
             <icon type="ios-paper"></icon>
-            <router-link tag="span" to="/css">
-              CSS学习
-            </router-link>
-        </menu-item>
-        <menu-item name="2">
-            <icon type="ios-people"></icon>
-            <router-link tag="span" to="/css">
-              javascript学习
-            </router-link>
-        </menu-item>
-        <!-- <submenu name="3">
-            <template slot="title">
-                <icon type="stats-bars"></icon>
-                统计分析
-            </template>
-            <menu-group title="使用">
-                <menu-item name="3-1">新增和启动</menu-item>
-                <menu-item name="3-2">活跃分析</menu-item>
-                <menu-item name="3-3">时段分析</menu-item>
-            </menu-group>
-            <menu-group title="留存">
-                <menu-item name="3-4">用户留存</menu-item>
-                <menu-item name="3-5">流失用户</menu-item>
-            </menu-group>
-        </submenu> -->
-        <menu-item name="4">
-            <icon type="settings"></icon>
-             <router-link tag="span" to="/css">
-              源码学习
-            </router-link>
-        </menu-item>
-         <menu-item name="5">
-            <icon type="settings"></icon>
-             <router-link tag="span" to="/css">
-              框架学习
+            <router-link tag="span" :to='item.url'>
+              {{item.name}}
             </router-link>
         </menu-item>
       </i-menu>
@@ -56,7 +24,17 @@
 export default {
   data () {
     return {
-      theme: 'light'
+      theme: 'light',
+      menuList: [{
+        name: 'Css 学习',
+        url: '/css'
+      }, {
+        name: 'Javascript 学习',
+        url: '/javascript'
+      }, {
+        name: '框架学习',
+        url: '/framework'
+      }]
     }
   },
   methods: {
